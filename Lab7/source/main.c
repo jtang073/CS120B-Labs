2bfe8b4b5af6fc673f04a5121105406de782ffeb
Jason Tang <jtang073@ucr.edu>
2020-05-09 02:25:19 -0700

    Created Lab7 and updated README.md

---
+++ Lab7/source/main.c
@@ -0,0 +1,23 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
